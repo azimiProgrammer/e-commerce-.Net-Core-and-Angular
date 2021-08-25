069da0cc2fd027ed7a88d8f5749303aaf6fc04d8
amir <azimi_programer@yahoo.com>
2021-08-26 01:19:40 +0430

    clean startup

--- API/ApplicationBuilder/IServiceCollectionExtentions.cs
+++ API/ApplicationBuilder/IServiceCollectionExtentions.cs
@@ -1,9 +1,12 @@
+using API.Errors;
 using Core.Contracts.Repository;
 using Infrastructure;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace API.ApplicationBuilder
 {
@@ -19,6 +22,28 @@ namespace API.ApplicationBuilder
         public static IServiceCollection AddRegisterDependencyInjection(this IServiceCollection services)
         {
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            return services;
+        }
+
+        public static IServiceCollection AddErrorConfig(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = ActionContext =>
+                {
+                    var errors = ActionContext.ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
             return services;
         }
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -36,24 +36,7 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5001");
                 })
             );
-
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.InvalidModelStateResponseFactory = ActionContext =>
-                {
-                    var errors = ActionContext.ModelState
-                    .Where(x => x.Value.Errors.Count > 0)
-                    .SelectMany(x => x.Value.Errors)
-                    .Select(x => x.ErrorMessage).ToArray();
-
-                    var errorResponse = new ApiValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
+            services.AddErrorConfig();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
