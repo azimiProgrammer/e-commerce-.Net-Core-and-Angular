9362024410bbe42df73485419c088367cbc84547
amir <azimi_programer@yahoo.com>
2021-08-28 14:56:44 +0430

    add httpclient

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,7 +33,7 @@ namespace API
             services.AddCors(opt =>
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
                 })
             );
             services.AddErrorConfig();
